f46176942fb0c7f7a80db5c54953345f23e12c6d
Pyonsu <pyonsu37@gmail.com>
2025-09-23 14:01:39 +0900

    吸血鬼

---
+++ vampire2.py
@@ -0,0 +1,10 @@
+name = 'Carol'
+age = 3000
+if name == 'Alice':
+    print('やぁ、Alice。')
+elif age < 12:
+    print('Ailceじゃないね、お嬢ちゃん。')
+elif age > 100:
+    print('Ailceじゃないね、お嬢ちゃん。')
+elif age > 2000:
+    print('Aliceはおまえのような吸血鬼ではない、吸血鬼め。')
