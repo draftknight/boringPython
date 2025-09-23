35b1141869e675731ba954c5325dbb22299d5f7f
Pyonsu <pyonsu37@gmail.com>
2025-09-23 14:44:15 +0900

    数当て

---
+++ guessTheNumber.py
@@ -0,0 +1,20 @@
+import random
+
+secret_number = random.randint(1,20)
+print('1から20までの数を当ててください。')
+
+for guess_token in range(1,7):
+    print('数を入力してください。')
+    guess = int(input())
+
+    if guess < secret_number:
+        print('あなたの推定値は小さいです。')
+    elif guess > secret_number:
+        print('あなたの推定値は大きいです。')
+    else:
+        break
+
+if guess == secret_number:
+    print('当たり!' + str(guess_token) + 'で当たりました!')
+else:
+    print('残念。正解は' + str(guess_token) + 'でした。')
